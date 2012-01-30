1dbdb7772361811ee3f4991805907aea156ad49d
Matt Mower <self@mattmower.com>
2012-01-30 15:21:02 +0000

    Initial Commit

---
+++ Statec/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Statec
+//
+//  Created by Matt Mower on 30/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
