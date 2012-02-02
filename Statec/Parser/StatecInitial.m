5429a5a570abefb4127d0a1c8889df34bba8cd72
Matt Mower <self@mattmower.com>
2012-02-02 11:28:14 +0000

    Updated the stupid __MyCompanyName__ copyright message.

--- Statec/Parser/StatecInitial.m
+++ Statec/Parser/StatecInitial.m
@@ -3,7 +3,7 @@
 //  Statec
 //
 //  Created by Matt Mower on 31/01/2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 SmartFish Software Ltd.. All rights reserved.
 //
 
 #import "StatecInitial.h"
